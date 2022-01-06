ce8131decc7926b291d52ab77709380352933649
Albina <Maestro1987>
2022-01-07 02:55:37 +0300

    add page object

--- src/test/java/onlc/dev/onlc.dev.tests/StudentRegistrationFormPageObjectsTest.java
+++ src/test/java/onlc/dev/onlc.dev.tests/StudentRegistrationFormPageObjectsTest.java
@@ -1,26 +1,11 @@
 package onlc.dev.onlc.dev.tests;
 
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.SelenideElement;
-import com.codeborne.selenide.selector.ByText;
-import com.codeborne.selenide.selector.WithText;
 import com.github.javafaker.Faker;
 import onlc.dev.pages.RegistrationPage;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
 
-import static com.codeborne.selenide.Selectors.byText;
-
-import java.io.File;
-import java.util.Locale;
-
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selenide.*;
-import static onlc.dev.onlc.dev.tests.TestData.userEmail;
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class StudentRegistrationFormPageObjectsTest {
     RegistrationPage registrationPage = new RegistrationPage();
@@ -30,7 +15,6 @@ public class StudentRegistrationFormPageObjectsTest {
     String lastName = faker.name().lastName();
     String streetAddress = faker.address().streetAddress();
     String userEmail = faker.internet().emailAddress();
-
 
 
     @BeforeAll
@@ -44,36 +28,29 @@ public class StudentRegistrationFormPageObjectsTest {
 
 
         registrationPage.openPage()
-        .typeFirstName(firstName)
-        .typeLastName(lastName)
-        .typeUserEmail(userEmail)
-        .genderCheck()
-        .userNumber()
-        .calendarComponent.setDate();
-        $("#hobbiesWrapper").click();
-        $("#subjectsInput").setValue("English").pressEnter();
-        $("#uploadPicture").uploadFromClasspath("img/1.jpg");
-        //$("#uploadPicture").uploadFile(new File("fixtures/EIEXH0aWkAMsRvR.jpg"));
-        $("#currentAddress").setValue(streetAddress);
-        $("#state").scrollTo().click();
-        //$(new WithText("Haryana")).shouldHave(text("H)"aryana")).click();
-        $(byText("Haryana")).shouldBe(visible).click();
-        $("#city").click();
-        $(new WithText("Panipat")).shouldHave(text("Panipat")).click();
-        $("#submit").click();
-        $(".modal-content");
-        $(new WithText("Thanks for submitting the form")).shouldHave(text("Thanks for submitting the form"));
-        assertEquals("Haryana Panipat", "Haryana Panipat");
-            /*$(".table-responsive").shouldHave(text("George Clooney"),text("test@tets.test"),
-                text("Male"),text("7674635847"),text("12 April,1999"),
-               text("English"),text("Reading"),text("1.jpg"),text("USA"),text("Haryana Panipat"));первый варик*/
-        registrationPage.checkResultsValue("Student Name", firstName + " " +lastName)
+                .typeFirstName(firstName)
+                .typeLastName(lastName)
+                .typeUserEmail(userEmail)
+                .genderCheck()
+                .userNumber()
+                .calendarComponent.setDate();
+        registrationPage.typeCheckboxSports()
+                .typeCheckboxSubjects()
+                .typeUploadPicture()
+                .typeCurrentAddress(streetAddress)
+                .typeScroll()
+                .typeStateCity()
+                .typeClick()
+                .typeCity()
+                .typeSubmit()
+                .typeModal()
+                .checkResultsValue("Student Name", firstName + " " + lastName)
                 .checkResultsValue("Student Email", userEmail)
                 .checkResultsValue("Gender", "Other")
                 .checkResultsValue("Mobile", "7733333333")
                 .checkResultsValue("Date of Birth", "30 July,2008")
                 .checkResultsValue("Subjects", "English")
-                .checkResultsValue("Hobbies", "Reading")
+                .checkResultsValue("Hobbies", "Sports")
                 .checkResultsValue("Picture", "1.jpg")
                 .checkResultsValue("Address", streetAddress)
                 .checkResultsValue("State and City", "Haryana Panipat");
--- src/test/java/onlc/dev/pages/RegistrationPage.java
+++ src/test/java/onlc/dev/pages/RegistrationPage.java
@@ -1,23 +1,34 @@
 package onlc.dev.pages;
 
-import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
 import onlc.dev.pages.components.CalendarComponent;
 
-
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class RegistrationPage {
-     SelenideElement
+    SelenideElement
             firstNameInput = $("#firstName"),
             lastNameInput = $("#lastName"),
+            checkboxGender = $(byText("Other")),
             resultsTable = $(".table-responsive"),
-            userEmailInput  = $("#userEmail");
+            userEmailInput = $("#userEmail"),
+            checkboxSports = $(byText("Sports")),
+            checkboxSubjects = $("#subjectsInput"),
+            uploadPicture = $("#uploadPicture"),
+            currentAddress = $("#currentAddress"),
+            scroll = $("#state"),
+            stateCity = $(byText("Haryana")),
+            click = $("#city"),
+            city = $("#stateCity-wrapper"),
+            submit = $("#submit"),
+            modal = $("#example-modal-sizes-title-lg");
 
-     public CalendarComponent calendarComponent = new CalendarComponent();
+
+    public CalendarComponent calendarComponent = new CalendarComponent();
 
 
     public RegistrationPage openPage() {
@@ -34,21 +45,72 @@ public class RegistrationPage {
         lastNameInput.setValue(value);
         return this;
     }
-    public RegistrationPage genderCheck  (){
-        $("#genterWrapper").$(byText("Other")).click();
+
+    public RegistrationPage genderCheck() {
+        checkboxGender.click();
         return this;
     }
-    public RegistrationPage typeUserEmail (String value) {
+
+    public RegistrationPage typeUserEmail(String value) {
         userEmailInput.setValue(value);
         return this;
     }
 
-    public RegistrationPage userNumber (){
+    public RegistrationPage userNumber() {
         $("#userNumber").setValue("7733333333");
         return this;
     }
 
 
+    public RegistrationPage typeCheckboxSports() {
+        checkboxSports.click();
+        return this;
+    }
+
+    public RegistrationPage typeCheckboxSubjects() {
+        checkboxSubjects.setValue("English").pressEnter();
+        return this;
+    }
+
+    public RegistrationPage typeUploadPicture() {
+        uploadPicture.uploadFromClasspath("img/1.jpg");
+        return this;
+    }
+
+    public RegistrationPage typeCurrentAddress(String value) {
+        currentAddress.setValue(value);
+        return this;
+    }
+
+    public RegistrationPage typeScroll() {
+        scroll.scrollTo().click();
+        return this;
+    }
+
+    public RegistrationPage typeStateCity() {
+        stateCity.shouldBe(visible).click();
+        return this;
+    }
+
+    public RegistrationPage typeClick() {
+        click.click();
+        return this;
+    }
+
+    public RegistrationPage typeCity() {
+        city.$(byText("Panipat")).click();
+        return this;
+    }
+
+    public RegistrationPage typeSubmit() {
+        submit.click();
+        return this;
+    }
+
+    public RegistrationPage typeModal() {
+        modal.shouldHave(text("Thanks for submitting the form"));
+        return this;
+    }
 
 
     public RegistrationPage checkResultsValue(String key, String value) {
